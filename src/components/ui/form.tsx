2b440f4f75ab2fe54f235dae4bd7673eee560920
takunagai <tak_na@icloud.com>
2025-03-22 15:46:35 +0900

    型エラーの修正

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import {
   Controller,
+  FormProvider,
+  useFormContext,
+} from "react-hook-form";
+import type {
   ControllerProps,
   FieldPath,
   FieldValues,
-  FormProvider,
-  useFormContext,
 } from "react-hook-form";
 
 import { cn } from "@/lib/utils";
