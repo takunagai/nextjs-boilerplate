19663c4d8dc49fcd03969dfba359fcd08b2618a7
takunagai <tak_na@icloud.com>
2025-04-29 09:45:39 +0900

    不要なコンテナを削除

--- src/app/(examples)/layout.tsx
+++ src/app/(examples)/layout.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent } from "@/components/ui/card";
+import { Container } from "@/components/ui/container";
 import { APP, FEATURES } from "@/lib/constants";
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
@@ -25,20 +26,22 @@ export default function ExamplesLayout({
 
 	return (
 		<>
-			<Card className="my-12 bg-amber-50">
-				<CardContent className="p-4 text-sm text-destructive">
-					<p>
-						これは開発用のサンプルページです。本番環境では表示されますが、実際のプロジェクトでは削除または修正してください。
-					</p>
-					<p className="mt-2">
-						src/lib/constants/app.ts の{` `}
-						<code>FEATURES.SHOW_EXAMPLES = false</code>、または、環境変数{` `}
-						<code>NEXT_PUBLIC_SHOW_EXAMPLES=false</code>
-						{` `}
-						で、このサンプルページを非表示にできます。
-					</p>
-				</CardContent>
-			</Card>
+			<Container width="lg">
+				<Card className="my-12 bg-amber-50">
+					<CardContent className="p-4 text-sm text-destructive">
+						<p>
+							これは開発用のサンプルページです。本番環境では表示されますが、実際のプロジェクトでは削除または修正してください。
+						</p>
+						<p className="mt-2">
+							src/lib/constants/app.ts の{` `}
+							<code>FEATURES.SHOW_EXAMPLES = false</code>、または、環境変数{` `}
+							<code>NEXT_PUBLIC_SHOW_EXAMPLES=false</code>
+							{` `}
+							で、このサンプルページを非表示にできます。
+						</p>
+					</CardContent>
+				</Card>
+			</Container>
 			<main className="flex-grow">{children}</main>
 		</>
 	);
