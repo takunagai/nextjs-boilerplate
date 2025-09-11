91d3ec4ad4e2e2c33e9d58ea4af75cf85843e462
takunagai <tak_na@icloud.com>
2025-09-11 22:01:23 +0900

    feat(cloudflare): 本番環境デプロイ成功とwrangler設定完了

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1757582145230,
+    __BUILD_TIMESTAMP_MS__: 1757595610166,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
